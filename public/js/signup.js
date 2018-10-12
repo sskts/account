520d34ceb3515e754517e0f96b9233132865338a
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-10-12 12:25:07 +0900

    SSKTS-1037  [スマフォアプリ]会員登録時パスワードを表示する機能追加

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,3 +1,30 @@
+$(function(){
+    $(document).on('click', '.eye, .eye-slash', visualizationPasswordToggle);
+});
+
+/**
+ * パスワード表示切り替え
+ * @param {Event} event 
+ */
+function visualizationPasswordToggle(event) {
+    event.preventDefault();
+    var isVisible = $(this).hasClass('eye-slash');
+    var target = $('input[name=password]');
+    if (isVisible) {
+        $('.eye-slash').removeClass('active');
+        $('.eye').addClass('active');
+        target.attr('type', 'password');
+    } else {
+        $('.eye').removeClass('active');
+        $('.eye-slash').addClass('active');
+        target.attr('type', 'text');
+    }
+}
+
+/**
+ * パスワード検証
+ * @param {'pc' | 'mobile'} screenSize 
+ */
 function checkPasswordMatch(screenSize) {
     var passwordPolicy = [];
     passwordPolicy.lowercase = "パスワードには小文字を含める必要があります";
