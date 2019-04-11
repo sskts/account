6bd59af06347ab06f5f049e81767a2673e97fbd0
Yu Iwasaki <yuiwasaki@loadoff.jp>
2019-04-11 12:46:10 +0900

    コピー修正

--- public/js/signup.js
+++ public/js/signup.js
@@ -160,14 +160,17 @@ function checkConfirmPasswordMatch(screenSize) {
 }
 
 function clickCopyToClipboard() {
-    if (copyTextToClipboard("@ticket-cinemasunshine.com")) {
-        $("#copy-to-clipboard>strong").html("✔　コピーしました")
-        $("#copy-to-clipboard").addClass("active");
-        setTimeout(function () {
-            $("#copy-to-clipboard>strong").html("ドメインをコピーする")
-            $("#copy-to-clipboard").removeClass("active");
-        }, 10000);
-    }
+    const range = document.createRange();
+    range.selectNode(document.getElementById("cinemasunshine-domain"))
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+  
+    $("#copy-to-clipboard>strong").html("✔　コピーしました")
+    $("#copy-to-clipboard").addClass("active");
+    setTimeout(function () {
+      $("#copy-to-clipboard>strong").html("ドメインをコピーする")
+      $("#copy-to-clipboard").removeClass("active");
+    }, 10000);
 }
 
 /**
