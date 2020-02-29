4686606a25e284fc04e89b4bd4c49d5029be6d26
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-03-01 02:14:41 +0900

    ### Added  - ユーザーネーム忘れモーダル追加

--- public/js/common.js
+++ public/js/common.js
@@ -16,3 +16,37 @@ function showLoading() {
     if ($('#username').val().length > 0 && $('#password').val().length > 0)
         $('.loading-cover, .loading').addClass('active');
 }
+
+/**
+ * モーダル
+ */
+var Modal = /** @class */ (function () {
+    function Modal(id) {
+        this.id = id;
+    }
+    Modal.prototype.getElement = function () {
+        return $('#' + this.id);
+    };
+    Modal.prototype.show = function () {
+        var _this = this;
+        $('body').append('<div class="modal-backdrop fade show"></div>')
+            .addClass('show');
+        this.getElement().one('click', function () {
+            console.log('modal')
+            _this.hide();
+        });
+        this.getElement().find('.modal-content').on('click', function (event) {
+            if ($(event.target).hasClass('close')) {
+                return;
+            }
+            event.stopPropagation();
+        });
+        this.getElement().addClass('show');
+    };
+    Modal.prototype.hide = function () {
+        this.getElement().removeClass('show');
+        $('.modal-backdrop').remove();
+        this.getElement().find('.modal-content').off();
+    };
+    return Modal;
+}());
